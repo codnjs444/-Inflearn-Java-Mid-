859c58be558df6e189928e455fdbfc48750a3740
codnjs444 <81910499+codnjs444@users.noreply.github.com>
2024-11-25 17:08:15 +0900

    24.11.25 : wrapper class 주요 메서드와 성능

---
+++ java-mid1/src/lang/wrapper/WrapperVsPrimitive.java
@@ -0,0 +1,30 @@
+package lang.wrapper;
+
+public class WrapperVsPrimitive {
+    public static void main(String[] args) {
+        int iterations = 1_000_000_000;
+        long startTime, endTime;
+
+        long sumPrimitve = 0;
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < iterations; i++) {
+            sumPrimitve += i;
+        }
+        endTime = System.currentTimeMillis();
+
+        System.out.println("sumPrimitive = " + sumPrimitve);
+        System.out.println("기본 자료형 long 실행 시간 : " + (endTime-startTime) + "ms");
+
+        Long sumWrapper = 0L;
+
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < iterations; i++) {
+            sumWrapper += i;
+        }
+        endTime = System.currentTimeMillis();
+
+        System.out.println("sumWrapper = " + sumWrapper);
+        System.out.println("래퍼클래스 long 실행 시간 : " + (endTime-startTime) + "ms");
+    }
+}
+
