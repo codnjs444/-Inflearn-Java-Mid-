1f75c79d5f140f15c78d8059385f63870b66d7be
codnjs444 <81910499+codnjs444@users.noreply.github.com>
2024-11-27 17:32:10 +0900

    24.11.27 : 지역 클래스

---
+++ java-mid1/src/nested/local/LocalOuterV4.java
@@ -0,0 +1,38 @@
+package nested.local;
+
+import java.lang.reflect.Field;
+
+public class LocalOuterV4 {
+    private final int outInstanceVar = 3;
+
+    public static void main(String[] args) {
+        LocalOuterV4 localOuterV3 = new LocalOuterV4();
+        Printer printer = localOuterV3.process(2);
+        printer.print();
+
+        System.out.println("필드 확인");
+        Field[] declaredFields = printer.getClass().getDeclaredFields();
+        for (Field declaredField : declaredFields) {
+            System.out.println("declaredField = " + declaredField);
+        }
+    }
+
+    public Printer process(int paramVar) {
+        int localVar = 1; // 지역 변수
+
+        class LocalPrinter implements Printer {
+            final int value = 0;
+
+            @Override
+            public void print() {
+                System.out.println("value = " + value);
+
+                // 인스턴스
+                System.out.println("localVar = " + localVar);
+                System.out.println("paramVar = " + paramVar);
+                System.out.println("outInstanceVar = " + outInstanceVar);
+            }
+        }
+        return new LocalPrinter();
+    }
+}
