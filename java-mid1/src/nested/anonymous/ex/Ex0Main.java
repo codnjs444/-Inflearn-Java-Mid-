747f3b7a0a7b694372db81a308bac36763811115
codnjs444 <81910499+codnjs444@users.noreply.github.com>
2024-11-27 18:01:20 +0900

    24.11.27 : 익명클래스 활용 진행 중

---
+++ java-mid1/src/nested/anonymous/ex/Ex0Main.java
@@ -0,0 +1,14 @@
+package nested.anonymous.ex;
+
+public class Ex0Main {
+    public static void powerOn(String name) {
+        System.out.println("프로그램 시작");
+        System.out.println(name);
+        System.out.println("프로그램 종료");
+    }
+
+    public static void main(String[] args) {
+        powerOn("helloJava");
+        powerOn("helloSpring");
+    }
+}
