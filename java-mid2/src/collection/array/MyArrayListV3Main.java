1860a8372daac45c55bd7db5dd26909517055dbd
codnjs444 <81910499+codnjs444@users.noreply.github.com>
2024-12-09 16:25:24 +0900

    24.12.09 : ArrayList <T>

---
+++ java-mid2/src/collection/array/MyArrayListV3Main.java
@@ -0,0 +1,23 @@
+package collection.array;
+
+public class MyArrayListV3Main {
+    public static void main(String[] args) {
+        MyArrayListV3 list = new MyArrayListV3();
+        list.add("a");
+        list.add("a");
+        list.add("a");
+        System.out.println(list);
+
+        System.out.println("addLast");
+        list.add(3, "addList");
+        System.out.println(list);
+
+        System.out.println("addFirst");
+        list.add(0, "addFirst");
+        System.out.println(list);
+
+        System.out.println("remove");
+        System.out.println("list.remove(4) = " + list.remove(4));
+        System.out.println(list);
+    }
+}
