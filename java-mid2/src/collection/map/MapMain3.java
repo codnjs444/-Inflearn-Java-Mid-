b657122ddd2742aaf31590b4223d4c63aec93012
codnjs444 <81910499+codnjs444@users.noreply.github.com>
2024-12-17 08:30:55 +0900

    24.12.17 : Map 기본 개념

---
+++ java-mid2/src/collection/map/MapMain3.java
@@ -0,0 +1,20 @@
+package collection.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapMain3 {
+    public static void main(String[] args) {
+        Map<String, Integer> studentMap = new HashMap<>();
+
+        studentMap.put("studentA", 50);
+        System.out.println("studentMap = " + studentMap);
+
+        if (!studentMap.containsKey("studentA")) {
+            studentMap.put("studentA", 100);
+        }
+        System.out.println("studentMap = " + studentMap);
+
+        studentMap.putIfAbsent("studentA",200);
+    }
+}
