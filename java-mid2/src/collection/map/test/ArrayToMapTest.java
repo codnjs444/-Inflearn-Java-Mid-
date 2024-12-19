dec20c1c1fd72a675038abcfdc0838062c847be2
codnjs444 <81910499+codnjs444@users.noreply.github.com>
2024-12-19 22:36:53 +0900

    init

--- java-mid2/src/collection/map/test/ArrayToMapTest.java
+++ java-mid2/src/collection/map/test/ArrayToMapTest.java
@@ -6,6 +6,7 @@ import java.util.Set;
 
 public class ArrayToMapTest {
     public static void main(String[] args) {
+        String[][] productArr = {{"Java", "10000"}, {"Spring", "20000"}, {"JPA", "30000"}};
         Map<String, Integer> productMap = new HashMap<>();
         for (String[] product : productArr) {
             productMap.put(product[0], Integer.valueOf(product[1]));
