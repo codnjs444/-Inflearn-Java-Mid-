2d2ec706fc6b512ffd557b82a741125f028a640d
codnjs444 <81910499+codnjs444@users.noreply.github.com>
2024-12-19 22:36:40 +0900

    24.12.19 : 문제 풀이 진행중

--- java-mid2/src/collection/iterable/MyArrayMain.java
+++ java-mid2/src/collection/iterable/MyArrayMain.java
@@ -6,8 +6,5 @@ public class MyArrayMain {
     public static void main(String[] args) {
         MyArray myArray = new MyArray(new int[]{1, 2, 3, 4});
         Iterator<Integer> iterator = myArray.iterator();
-        for (Integer integer : iterator) {
-            System.out.println("integer = " + integer);
-        }
     }
 }
---
+++ java-mid2/src/collection/map/test/ArrayToMapTest.java
@@ -0,0 +1,19 @@
+package collection.map.test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class ArrayToMapTest {
+    public static void main(String[] args) {
+        Map<String, Integer> productMap = new HashMap<>();
+        for (String[] product : productArr) {
+            productMap.put(product[0], Integer.valueOf(product[1]));
+        }
+        Set<Map.Entry<String, Integer>> entries = productMap.entrySet();
+        for (Map.Entry<String, Integer> entry : entries) {
+            System.out.println("entry = " + entry);
+        }
+    }
+
+}
