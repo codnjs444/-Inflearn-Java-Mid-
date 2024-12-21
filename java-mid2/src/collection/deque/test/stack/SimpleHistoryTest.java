1dd59a840024763cdfcc4080ac83f79935861b3c
codnjs444 <81910499+codnjs444@users.noreply.github.com>
2024-12-21 14:14:19 +0900

    24.12.21 : 문제풀이 (Stack, Queue)

---
+++ java-mid2/src/collection/deque/test/stack/SimpleHistoryTest.java
@@ -0,0 +1,21 @@
+package collection.deque.test.stack;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class SimpleHistoryTest {
+    public static void main(String[] args) {
+        Deque<String> stack = new ArrayDeque<>();
+        stack.push("youtube");
+        stack.push("google");
+        stack.push("facebook");
+
+        System.out.println(stack.pop());
+        System.out.println("stack = " + stack);
+        System.out.println(stack.pop());
+        System.out.println("stack = " + stack);
+        System.out.println(stack.pop());
+
+
+    }
+}
